a2ae2aa8fc6eeb67163b1f2f37fee36f524161d7
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-26 16:24:41 +0000

    Adding the blog posts json to the home function to be rendered as a json in the html file

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url="https://api.npoint.io/c790b4d5cab58020d391"
+    
+    response=requests.get(blog_url)
+    blog_post_json=response.json()
+
+    return render_template("index.html", blog_posts=blog_post_json)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
