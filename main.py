243c0b1a58c31155b5918c2abe34bb0a789845f4
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-27 09:53:35 +0000

    Made a "Read" anchor tag at the end of each blog post preview link to a page with the entire blog - title, subtitle and body. The individual blog posts should live at the path: URL/post/blog_id

--- main.py
+++ main.py
@@ -11,16 +11,20 @@ blog_post_json=response.json()
 
 @app.route('/')
 def home():
-    blog_url="https://api.npoint.io/c790b4d5cab58020d391"
+    # blog_url="https://api.npoint.io/c790b4d5cab58020d391"
     
-    response=requests.get(blog_url)
-    blog_post_json=response.json()
+    # response=requests.get(blog_url)
+    # blog_post_json=response.json()
 
     return render_template("index.html", blog_posts=blog_post_json)
 
 @app.route('/<blog_number>')
 def blog(blog_number):
-    pass
+    blog_number=int(blog_number) - 1
+    if int(blog_number) > len(blog_post_json):
+        pass
+    else:
+        return render_template("post.html", blog_posts=blog_post_json,number=blog_number)
 
 
 if __name__ == "__main__":
