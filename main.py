7fcf3f3c6b317b2fd6f59e2beb4ba578765dadfa
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-02-27 09:27:31 +0000

    Using the API for our blog posts we created on n:Point, render all the blogs' title and subtitles on the home page.

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import requests
 
 app = Flask(__name__)
 
+blog_url="https://api.npoint.io/c790b4d5cab58020d391"
+
+response=requests.get(blog_url)
+blog_post_json=response.json()
+
 @app.route('/')
 def home():
     blog_url="https://api.npoint.io/c790b4d5cab58020d391"
@@ -13,6 +18,9 @@ def home():
 
     return render_template("index.html", blog_posts=blog_post_json)
 
+@app.route('/<blog_number>')
+def blog(blog_number):
+    pass
 
 
 if __name__ == "__main__":
